7d9e2951b2568dfade073230e53198d84b7bf7e4
Andrew Fausey <andrew.fausey@gmail.com>
2020-03-24 10:42:09 -0400

    Attempt number 2, swtiched formatting of command lines.

---
+++ Manual Commit and Push Practice.R
@@ -0,0 +1,9 @@
+
+print"Again, I am practicing my pushing skills to Githib"
+
+print"I really hope this works"
+
+git commit -m "For practice of manualing adding a commit to the command line and for
+pushing to Github"
+
+print"attempt numero dos"
